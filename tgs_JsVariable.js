9da02b33ffe920f8bdbdd93af25538958a8e3699
Umi Khaerun nisa <160298671+ukhaerunnisa@users.noreply.github.com>
2024-04-01 22:48:40 +0700

    Tugas Eduwork JS Variable

---
+++ tgs_JsVariable.js
@@ -0,0 +1,10 @@
+// Penjumlahan perkode
+let x = "10" + 10 +10;
+console.log(x);
+let y = 10 + 10 +"10";
+console.log(y);
+
+// Penjumlahan kode x + y
+let x1 = "10" + 10 +10;
+let y2 = 10 + 10 +"10";
+console.log(x1 + y2);
